534611c9f301b79442b0df8a592fffe15797a912
EderH3nr963 <ederhenriquevicentejust18@gmail.com>
2024-07-08 00:13:17 -0300

    Teste

---
+++ scripts/enviarEmail.php
@@ -0,0 +1,7 @@
+<?php 
+require_once dirname(__DIR__)."/app/config.php";
+require_once APP_PATH."/models/UsuarioModel.php";
+require_once APP_PATH."/models/PlantacaoModel.php";
+require_once APP_PATH."/helpers/UsaurioHelper.php";
+require_once APP_PATH."/helpers/PlantacaoHelper.php";
+
